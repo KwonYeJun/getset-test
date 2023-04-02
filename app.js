cc85269e77d14768f09c34d550349cf20c380f6a
gwonyejun <kyjuni1059@gmail.com>
2023-04-02 20:29:24 +0900

    마무리

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function User(name, birthday) {
   // age는 현재 날짜와 생일을 기준으로 계산됩니다.
   Object.defineProperty(this, "age", {
     get() {
-      let todayYear = new Date().getFullYear();
-      return todayYear - this.birthday.getFullYear();
+      let todayYear = new Date().getFullYear;
+      return todayYear - this.birthday.getDay();
     }
   });
 }
